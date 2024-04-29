b2b34eb0f280492827f2f4e97cb2994a1f3ee2ca
Chelsea Brooks <cbrooks6212@gmail.com>
2024-04-28 23:27:23 -0400

    anotherupdate

--- scripts/dockList.js
+++ scripts/dockList.js
@@ -6,6 +6,18 @@ document.addEventListener(
         const itemClicked = clickEvent.target;
         if (itemClicked.dataset.type === "dock"){
             const dockId = itemClicked.dataset.dockId;
+        const haulers = getHaulers();
+        let docksHaulingShips =[];
+        for ( const hauler of haulers) {
+            if (haulers.dockId === parseInt(dockId)) {
+            docksHaulingShips.push(hauler.name)
+            }
+        }    
+        if (docksHaulingShips.length === 0){
+            window.alert(`${itemClicked.dataset.location} is currently unloading nothing`);
+        } else {
+            window.alert(`${itemClicked.dataset.location} is currently unloading ${docksHaulingShips.join(",")}`)
+        }
 
         }
 
@@ -20,6 +32,7 @@ export const dockList = () => {
     for (const dock of docks) {
         // Convert each dock object to an <li> and append to the docksHTML string
         docksHTML += `<li data-id="${dock.id}"
+                           data-type="dock"
                            data-location="${dock.location}"
                            data-volume="${dock.volume}">
                            ${dock.location}
--- scripts/shippingList.js
+++ scripts/shippingList.js
@@ -1,48 +1,46 @@
-import { getHaulers, getShippingShips } from "./database.js"
+import { getHaulers, getShippingShips } from "./database.js";
 
-document.addEventListener(
-    "click",
-    (clickEvent) => {
-        const itemClicked = clickEvent.target
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
 
-        // Was a shipping ship list item clicked?
-        if (itemClicked.dataset.type === "shippingShip") {
-            // Get the haulerId value of the shipping ship clicked
-        const shipHaulerId = itemClicked.dataset.haulerId
-            // Define a default object for the found hauler
-            let haulingShip = { name: "Incorrect" }
-            // Iterate the array of hauler objects
-            const haulers = getHaulers();
-            for (const hauler of haulers){
-            // Does the haulerId foreign key match the id of the current hauler?
-             if (parseInt(shipHaulerId) === hauler.id) {
-            // Reassign the value of `haulingShip` to the current hauler
-            haulingShip = `${hauler.name}`
-             }      
-            }
-            // Show an alert to the user with this format...
-            window.alert(`${itemClicked.dataset.name} is being hauled by ${haulingShip}`)
-                }
-                
-    });
-
+  // Was a shipping ship list item clicked?
+  if (itemClicked.dataset.type === "shippingShip") {
+    // Get the haulerId value of the shipping ship clicked
+    const shipHaulerId = itemClicked.dataset.haulerId;
+    // Define a default object for the found hauler
+    let haulingShip = { name: "Incorrect" };
+    // Iterate the array of hauler objects
+    const haulers = getHaulers();
+    for (const hauler of haulers) {
+      // Does the haulerId foreign key match the id of the current hauler?
+      if (parseInt(shipHaulerId) === hauler.id) {
+        // Reassign the value of `haulingShip` to the current hauler
+        haulingShip = `${hauler.name}`;
+      }
+    }
+    // Show an alert to the user with this format...
+    window.alert(
+      `${itemClicked.dataset.name} is being hauled by ${haulingShip}`
+    );
+  }
+});
 
 export const shippingShipsList = () => {
-    const shippingShips = getShippingShips()
+  const shippingShips = getShippingShips();
 
-    let shippingShipsHTML = "<ul>"
+  let shippingShipsHTML = "<ul>";
 
-    for (const shippingShip of shippingShips) {
-        // Convert each dock object to an <li> and append to the docksHTML string
-        shippingShipsHTML += `<li data-id="${shippingShip.id}"
+  for (const shippingShip of shippingShips) {
+    // Convert each dock object to an <li> and append to the docksHTML string
+    shippingShipsHTML += `<li data-id="${shippingShip.id}"
                                   data-type="shippingShip"  
                                   data-name="${shippingShip.name}"
-                                  data-haulerId="${shippingShip.haulerId}">
+                                  data-hauler-id="${shippingShip.haulerId}">
                                   ${shippingShip.name}
-                                  </li>`
-    }
+                                  </li>`;
+  }
 
-    shippingShipsHTML += "</ul>"
+  shippingShipsHTML += "</ul>";
 
-    return shippingShipsHTML
-}
+  return shippingShipsHTML;
+};
